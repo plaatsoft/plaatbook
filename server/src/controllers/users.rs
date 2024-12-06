c91a4e2628f6be76f850b110d54a7390421e952a
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-12-06 10:44:21 +0100

    Server: Replace axum and sqlx with bplaat/crates

--- server/src/controllers/users.rs
+++ server/src/controllers/users.rs
@@ -4,68 +4,72 @@
  * SPDX-License-Identifier: MIT
  */
 
-use axum::extract::{Path, State};
-use axum::http::StatusCode;
-use axum::response::IntoResponse;
-use axum::Json;
-use sqlx::{Pool, Sqlite};
+use anyhow::Result;
+use chrono::Utc;
+use http::{Request, Response, Status};
+use router::Path;
 use uuid::Uuid;
 
-use super::not_found;
-use crate::models::user::User;
+use crate::controllers::not_found;
+use crate::models::User;
+use crate::Context;
 
-pub async fn users_index(State(database): State<Pool<Sqlite>>) -> impl IntoResponse {
-    Json(
-        sqlx::query_as!(
-            User,
-            "SELECT id as \"id: uuid::Uuid\", username, email, password, created_at, updated_at FROM users"
-        )
-        .fetch_all(&database)
-        .await
-        .unwrap(),
-    )
+pub fn users_index(_: &Request, ctx: &Context, _: &Path) -> Result<Response> {
+    // Get users
+    let users = ctx
+        .database
+        .query::<User>(format!("SELECT {} FROM users", User::columns()), ())?
+        .collect::<Result<Vec<_>, sqlite::Error>>()?;
+    Ok(Response::new().json(users))
 }
 
-pub async fn users_store(State(database): State<Pool<Sqlite>>) -> impl IntoResponse {
+pub fn users_store(_: &Request, ctx: &Context, _: &Path) -> Result<Response> {
     // Create a new user
     let user = User {
         id: Uuid::now_v7(),
         username: "plaatsoft".to_string(),
         email: "info@plaatsoft.nl".to_string(),
         password: "password".to_string(),
-        created_at: chrono::Utc::now().naive_utc(),
-        updated_at: chrono::Utc::now().naive_utc(),
+        created_at: Utc::now(),
+        updated_at: Utc::now(),
     };
-    sqlx::query!(
-            "INSERT INTO users (id, username, email, password, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)",
-            user.id,
-            user.username,
-            user.email,
-            user.password,
-            user.created_at,
-            user.updated_at,
-        )
-        .fetch_optional(&database)
-        .await
-        .unwrap();
+    ctx.database
+        .query::<()>(
+            format!(
+                "INSERT INTO users ({}) VALUES ({})",
+                User::columns(),
+                User::params()
+            ),
+            user.clone(),
+        )?
+        .next();
 
-    (StatusCode::CREATED, Json(user)).into_response()
+    Ok(Response::new().json(user))
 }
 
-pub async fn users_show(
-    State(database): State<Pool<Sqlite>>,
-    Path(user_id): Path<Uuid>,
-) -> impl IntoResponse {
-    if let Ok(user) =
-        sqlx::query_as!(
-            User,
-            "SELECT id as \"id: uuid::Uuid\", username, email, password, created_at, updated_at FROM users WHERE id = ?"
-        ,user_id)
-        .fetch_one(&database)
-        .await
-    {
-       (StatusCode::OK, Json(user)).into_response()
+pub fn users_show(req: &Request, ctx: &Context, path: &Path) -> Result<Response> {
+    // Parse user id from url
+    let user_id = match path.get("user_id").expect("Should be some").parse::<Uuid>() {
+        Ok(id) => id,
+        Err(_) => {
+            return Ok(Response::new()
+                .status(Status::BadRequest)
+                .body("400 Bad Request"));
+        }
+    };
+
+    // Get user
+    let user = ctx
+        .database
+        .query::<User>(
+            format!("SELECT {} FROM users WHERE id = ? LIMIT 1", User::columns()),
+            user_id,
+        )?
+        .next();
+
+    if let Some(Ok(user)) = user {
+        Ok(Response::new().json(user))
     } else {
-        not_found().await.into_response()
+        not_found(req, ctx, path)
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -4,39 +4,107 @@
  * SPDX-License-Identifier: MIT
  */
 
-use std::net::Ipv4Addr;
+use std::net::{Ipv4Addr, TcpListener};
+use std::sync::Arc;
 
 use anyhow::Result;
-use axum::routing::{get, post};
-use axum::Router;
-use controllers::users::{users_show, users_store};
-use sqlx::SqlitePool;
-use tower_http::cors::CorsLayer;
+use chrono::Utc;
+use http::Response;
+use router::Router;
+use uuid::Uuid;
 
-use crate::controllers::users::users_index;
+use crate::controllers::users::{users_index, users_show, users_store};
 use crate::controllers::{home, not_found};
+use crate::models::User;
 
 mod controllers;
 mod models;
 
-const HTTP_PORT: u16 = 8080;
-const DATABASE_URL: &str = "sqlite://database.db";
+const DATABASE_PATH: &str = "database.db";
+const HTTP_PORT: u16 = 8000;
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let database = SqlitePool::connect(DATABASE_URL).await?;
+// MARK: Database
+#[derive(Clone)]
+struct Context {
+    database: Arc<sqlite::Connection>,
+}
 
-    let app = Router::new()
-        .route("/", get(home))
-        // Users
-        .route("/users", get(users_index))
-        .route("/users", post(users_store))
-        .route("/users/:user_id", get(users_show))
-        .fallback(not_found)
-        .layer(CorsLayer::new().allow_origin(tower_http::cors::Any))
-        .with_state(database);
+fn open_database() -> Result<sqlite::Connection> {
+    // Open database and create tables
+    let database = sqlite::Connection::open(DATABASE_PATH)?;
+    database.execute(
+        "CREATE TABLE IF NOT EXISTS users (
+            id BLOB PRIMARY KEY,
+            username TEXT UNIQUE NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            created_at TIMESTAMP NOT NULL,
+            updated_at TIMESTAMP NOT NULL
+        )",
+    )?;
 
-    let listener = tokio::net::TcpListener::bind((Ipv4Addr::UNSPECIFIED, HTTP_PORT)).await?;
-    axum::serve(listener, app).await?;
-    Ok(())
+    // Seed database
+    let users_count = database
+        .query::<i64>("SELECT COUNT(id) FROM users", ())?
+        .next()
+        .expect("Should be some")?;
+    if users_count == 0 {
+        let admin = User {
+            id: Uuid::now_v7(),
+            username: "admin".to_string(),
+            email: "info@plaatsoft.nl".to_string(),
+            password: "password".to_string(),
+            created_at: Utc::now(),
+            updated_at: Utc::now(),
+        };
+        database
+            .query::<()>(
+                format!(
+                    "INSERT INTO users ({}) VALUES ({})",
+                    User::columns(),
+                    User::params()
+                ),
+                admin,
+            )?
+            .next();
+    }
+
+    Ok(database)
+}
+
+// MARK: Main
+fn main() {
+    let ctx = Context {
+        database: Arc::new(open_database().expect("Can't open database")),
+    };
+
+    let router = Arc::new(
+        Router::<Context>::new()
+            .get("/", home)
+            // Users
+            .get("/users", users_index)
+            .post("/users", users_store)
+            .get("/users/:user_id", users_show)
+            // Not found
+            .fallback(not_found),
+    );
+
+    println!("Server is listening on: http://localhost:{}/", HTTP_PORT);
+    let listener = TcpListener::bind((Ipv4Addr::UNSPECIFIED, HTTP_PORT))
+        .unwrap_or_else(|_| panic!("Can't bind to port: {}", HTTP_PORT));
+    http::serve(listener, move |req| {
+        // Error middleware
+        let res = match router.next(req, &ctx) {
+            Ok(res) => res,
+            Err(err) => {
+                println!("Error: {:?}", err);
+                Response::new()
+                    .status(http::Status::InternalServerError)
+                    .body("500 Internal Server Error")
+            }
+        };
+
+        // Cors middleware
+        res.header("Access-Control-Allow-Origin", "*")
+    });
 }
