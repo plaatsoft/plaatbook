e15378d783c3a9fc3fc4572a07b87fdfb202d7a7
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-12-08 18:03:37 +0100

    Web: Fix auth issues with pages

---
+++ web/src/pages/notfound.tsx
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024 PlaatSoft
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+export function NotFound() {
+    return (
+        <div className="section">
+            <h2 className="title">404 Not Found</h2>
+        </div>
+    );
+}
