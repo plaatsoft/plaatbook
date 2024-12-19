075d5e07f221edae25c448db07f90a204c014df0
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-12-19 18:48:31 +0100

    Web: Add create post form to user show page

---
+++ web/src/components/create-post.tsx
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2024 PlaatSoft
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+import { useState } from 'preact/hooks';
+import { PostsService, $refreshPosts } from '../services/posts.service.ts';
+import { Errors } from '../models/errors.ts';
+import { Field } from '../components/field.tsx';
+
+export function CreatePost() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [text, setText] = useState('');
+    const [errors, setErrors] = useState<Errors>({});
+
+    const createPost = async (event: SubmitEvent) => {
+        event.preventDefault();
+        setIsLoading(true);
+        setErrors({});
+        const errors = await PostsService.getInstance().create(text);
+        setIsLoading(false);
+        if (errors === null) {
+            setText('');
+            $refreshPosts.value = $refreshPosts.value + 1;
+        } else {
+            setErrors(errors);
+        }
+    };
+
+    return (
+        <>
+            <form className="media" onSubmit={createPost}>
+                <div className="media-left">
+                    <img className="image is-64x64" src="/images/avatar.svg" />
+                </div>
+                <div className="media-content">
+                    <Field
+                        name="text"
+                        type="textarea"
+                        placeholder="What's on your mind?"
+                        value={text}
+                        setValue={setText}
+                        error={errors.text?.join(', ')}
+                        disabled={isLoading}
+                    />
+
+                    <div className="field">
+                        <button className="button is-link" type="submit">
+                            Post
+                        </button>
+                    </div>
+                </div>
+            </form>
+            <hr />
+        </>
+    );
+}
