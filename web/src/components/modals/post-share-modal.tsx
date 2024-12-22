6eb3a57a88afbf62d8d96cefff87a34fcb096b8a
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-12-22 09:11:54 +0100

    Web: Add post share button

---
+++ web/src/components/modals/post-share-modal.tsx
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2024 PlaatSoft
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+import { Post } from '../../models/post.ts';
+import { CopyIcon } from '../icons.tsx';
+
+export function PostShareModal({ post, onConfirm }: { post: Post; onConfirm: (updatedPost: null) => void }) {
+    const copyUrl = async () => {
+        await navigator.clipboard.writeText(`${window.location.host}/posts/${post.id}`);
+    };
+
+    return (
+        <div className="modal is-active">
+            <div className="modal-background" onClick={() => onConfirm(null)}></div>
+            <div className="modal-card">
+                <header className="modal-card-head">
+                    <p className="modal-card-title">Share post</p>
+                    <button className="delete" aria-label="close" onClick={() => onConfirm(null)}></button>
+                </header>
+                <footer className="modal-card-foot">
+                    <div className="buttons">
+                        <button type="submit" className="button is-link" onClick={copyUrl}>
+                            <CopyIcon className="mr-2" />
+                            Copy url
+                        </button>
+                        <button className="button" onClick={() => onConfirm(null)}>
+                            Cancel
+                        </button>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    );
+}
